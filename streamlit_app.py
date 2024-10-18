daa0a328a56f11ffc53748df82ae882af937c1a2
gungwah <gungwah.amitaba@gmail.com>
2024-10-18 19:02:29 -0400

    number

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,13 @@ st.markdown("made by **Agung Ngurah Oka Abhina** BUID 15387312. :medal: :medal: 
 prompt = st.text_input("Enter your prompt:")
 
 # Textbox for number of tokens
-num_tokens = st.number_input("Number of tokens:", min_value=10, max_value=250, value=20)
+num_tokens = st.number_input("Number of tokens:", value=20)
 
 # Initialize the GPT-2 pipeline
 generator = pipeline('text-generation', model='gpt2')
 
 # Function to generate text
 def generate_text(prompt, num_tokens, creativity):
-    set_seed(42)  # For reproducibility, optional
     if creativity == "High":
         # Higher temperature for more creative output
         return generator(prompt, max_length=num_tokens, temperature=1.0, do_sample=True)[0]['generated_text']
@@ -29,13 +28,12 @@ def generate_text(prompt, num_tokens, creativity):
         # Lower temperature for more predictable output
         return generator(prompt, max_length=num_tokens, temperature=0.7, do_sample=True)[0]['generated_text']
 
-# Button to generate text
+
 if st.button("Generate Text"):
-    # Generate responses with different creativity levels
+   
     creative_response = generate_text(prompt, num_tokens, "High")
     predictable_response = generate_text(prompt, num_tokens, "Low")
 
-    # Display the responses
     st.header("Creative Response:")
     st.write(creative_response)
 
